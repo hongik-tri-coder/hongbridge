9ff71f7b2bb3234ba2f05e97d3ef3c00b3369813
tasu1052 <tasu1052@naver.com>
2025-10-15 22:12:41 +0900

    푸시알림 기능 구현

--- backend/src/main/java/com/example/HongBridge/repository/ScheduleRepository.java
+++ backend/src/main/java/com/example/HongBridge/repository/ScheduleRepository.java
@@ -2,12 +2,17 @@ package com.example.HongBridge.repository;
 
 import com.example.HongBridge.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     // Member의 studentId 기준으로 일정 조회
     List<Schedule> findByUserStudentId(String studentId);
+    @Query("SELECT s FROM Schedule s WHERE DATE(s.start) = :date")
+    List<Schedule> findByStartDate(@Param("date") LocalDate date);
 }
